31e9d45269b4ae6fae5c059b836670d106c709cb
Aman <amsingh714@gmail.com>
2019-01-16 18:30:54 -0500

    added seeds, able to GET and POST

--- index.js
+++ index.js
@@ -10,6 +10,65 @@ server.use(express.json());
 const db = knex(knexConfig.development);
 
 
+server.get('/', (req, res) => {
+    res.send('API IS WORKING');
+});
+
+
+// GET - READ 
+server.get("/api/cohorts", (req,res) => {
+    db("cohorts")
+    .then(cohorts => {
+        res.status(200).json(cohorts);
+    })
+    .catch(err => {
+        releaseEvents.status(500).json({ error: "The cohorts could not be retrieved." })
+    })
+})
+
+// GET - READ with specific ID
+server.get("/api/cohorts/:id", (req, res) => {
+    const id = req.params.id;
+
+    db("cohorts")
+    .where({ id: id })
+    .then(cohort => {
+        if (cohort) {
+            res.status(200).json(cohort)
+        } else {
+            res.status(404).json({ error: "The cohort with the specified ID was not found."})
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ error: "The cohort with the specified ID does not exist." })
+    })
+})
+
+// POST - ADD 
+server.post("/api/cohorts", (req, res) => {
+    const cohort = req.body;
+
+    if (!cohort.name) {
+        res.status(400).json({
+            error: "Please provide a name for the cohort"
+          });
+    } else {
+        db("cohorts")
+        .insert(cohort)
+        .then(result => {
+            res.status(201).json(result)
+        })
+        .catch(err => {
+            res.status(500).json({
+              error: "There was an error while saving the cohort to the database."
+            });
+          });
+    }
+})
+
+
+
+
 
 
 server.listen(8000, () => console.log("Server up on 8000"));

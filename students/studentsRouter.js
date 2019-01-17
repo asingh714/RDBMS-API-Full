7ce49413bed9457443581b12d0350f24e5c4eb74
Aman <amsingh714@gmail.com>
2019-01-17 12:55:46 -0500

    finished stretch

--- students/studentsRouter.js
+++ students/studentsRouter.js
@@ -23,7 +23,7 @@ router.get("/:id", (req, res) => {
     .where({ id: id })
     .then(student => {
       if (student) {
-        res.status(200).json(student);
+        res.status(200).json({student});
       } else {
         res
           .status(404)
@@ -34,6 +34,66 @@ router.get("/:id", (req, res) => {
       res
         .status(500)
         .json({ error: "The student with the specified ID does not exist." });
+    });
+});
+
+// POST - ADD
+router.post("/", (req, res) => {
+  const student = req.body;
+
+  if (!student.name || !student.cohort_id) {
+    res.status(400).json({
+      error: "Please provide a name and cohort_id for the student."
+    });
+  } else {
+    db("students")
+      .insert(student)
+      .then(result => {
+        res.status(201).json(result);
+      })
+      .catch(err => {
+        res.status(500).json({
+          error: "There was an error while saving the student to the database."
+        });
+      });
+  }
+});
+
+// PUT - UPDATE
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  const changes = req.body;
+  if (!changes.name || !changes.cohort_id) {
+    res
+      .status(400)
+      .json({ error: "Please provide a cohort ID and name for the student." });
+  }
+
+  db("students")
+    .where({ id: id })
+    .update(changes)
+    .then(student => {
+      res.status(200).json(student);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: "The student information could not be modified."
+      });
+    });
+});
+
+// DELETE
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+
+  db("students")
+    .where({ id: id })
+    .del()
+    .then(count => {
+      res.status(200).json(count);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "The student could not be removed." });
     });
 });
 

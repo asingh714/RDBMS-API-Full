3f6bfc7b8ca98687c54f0e669155625b799e0786
Aman <amsingh714@gmail.com>
2019-03-27 16:08:16 -0400

    able to GET cohort based on ID

--- cohorts/cohorts-router.js
+++ cohorts/cohorts-router.js
@@ -17,4 +17,25 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  db("cohorts")
+    .where({ id })
+    .first()
+    .then(cohort => {
+      if (cohort) {
+        res.status(200).json(cohort);
+      } else {
+        res
+          .status(404)
+          .json({ error: "The cohort with the specified ID does not exist." });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "The cohort with the specified ID could not be retrieved"
+      });
+    });
+});
+
 module.exports = router;
